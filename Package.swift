ff086c5dea9ff81e7f5e8ddee4ade8a995a140cf
Eric Yanush <eric.yanush@sasktelinternational.com>
2019-09-24 11:38:30 -0600

    remove the testing target, as it results in an a duplicated target names when imported into the testing package. Need to find a better way of handling this

--- Package.swift
+++ Package.swift
@@ -12,9 +12,4 @@ pkg.targets = [
     .target(name: "ProcedureKit"),
     .target(name: "ProcedureKitLocation", dependencies: ["ProcedureKit"]),
     .target(name: "ProcedureKitNetwork", dependencies: ["ProcedureKit"]),
-    .target(name: "TestingProcedureKit", dependencies: ["ProcedureKit"]),
-    .testTarget(name: "ProcedureKitTests", dependencies: ["ProcedureKit", "TestingProcedureKit"]),
-    .testTarget(name: "ProcedureKitStressTests", dependencies: ["ProcedureKit", "TestingProcedureKit"]),
-    .testTarget(name: "ProcedureKitLocationTests", dependencies: ["ProcedureKitLocation", "TestingProcedureKit"]),
-    .testTarget(name: "ProcedureKitNetworkTests", dependencies: ["ProcedureKitNetwork", "TestingProcedureKit"]),
 ]
